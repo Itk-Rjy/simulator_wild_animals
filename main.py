0bb3dbd728e020933c9ed990d3460a6bf12b5015
Itk-Rjy <kshelyakov8@gmail.com>
2023-08-31 16:55:48 +0300

    simulator_wild_animals

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import time
 from tiger import *
 from hare import *
 
@@ -7,10 +8,39 @@ class Game():
         self.tiger = Tiger()
         self.hare = Hare()
         self.hare2 = Hare()
+        self.create_field()
+        self.main_game()
+        
+        
+        self.window.mainloop()
 
-    def run(self):
-        print(123456)
+    def main_game(self):
+        while True:
+            x, y = self.tiger.pos
+            self.field[x][y]['text'] = 't'
+            self.window.update()
+            time.sleep(1)
+            self.field[x][y]['text'] = ''
+            self.window.update()
+            self.tiger.next_pos()
+
+    def create_field(self):
+        self.window = tk.Tk()
+        self.window.title('Simulator_wild_animals')
+        self.window.geometry('300x300')
+        self.field = []
+        for i in range(5):
+            line = []
+            for j in range(5):
+                lb = tk.Label(self.window, text = f'{i+1}-{j+1}')
+                lb.grid(row = i, column = j)
+                line.append(lb)
+            self.field.append(line)
+        
 
 if __name__ == '__main__':
     game = Game()
     game.run()
+
+    
+        
--- tiger.py
+++ tiger.py
@@ -1,2 +1,33 @@
+import random as rd
+
 class Tiger():
-    pass
+    def __init__(self):
+        self.state = ['find', 'attack', 'go_home']
+        self.pos = [0,0]
+
+    def next_pos(self):
+        if rd.random()>0.5:
+            x = self.pos[0]
+            if rd.random()>0.5:
+                x += 1
+            else:
+                x -= 1
+            if x == 5:
+                x = 3
+            if x == -1:
+                x = 1
+            self.pos[0] = x
+        else:
+            y = self.pos[1]
+            if rd.random()>0.5:
+                y += 1
+            else:
+                y -= 1
+            if y == 5:
+                y = 3
+            if y == -1:
+                y = 1
+            self.pos[1] = y
+            
+
+            
